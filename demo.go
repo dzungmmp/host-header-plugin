d593f480a475dfa9ea71b5e0cfcd23798d5439f7
Dzung Tran <dung.tran@missmp.eu>
2023-11-03 17:39:34 +0700

    Add test host header

--- demo.go
+++ demo.go
@@ -2,11 +2,9 @@
 package plugindemo
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
 // Config the plugin configuration.
@@ -23,10 +21,10 @@ func CreateConfig() *Config {
 
 // Demo a Demo plugin.
 type Demo struct {
-	next     http.Handler
-	headers  map[string]string
-	name     string
-	template *template.Template
+	next    http.Handler
+	headers map[string]string
+	name    string
+	// template *template.Template
 }
 
 // New created a new Demo plugin.
@@ -36,31 +34,19 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 	}
 
 	return &Demo{
-		headers:  config.Headers,
-		next:     next,
-		name:     name,
-		template: template.New("demo").Delims("[[", "]]"),
+		headers: config.Headers,
+		next:    next,
+		name:    name,
+		// template: template.New("demo").Delims("[[", "]]"),
 	}, nil
 }
 
 func (a *Demo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	for key, value := range a.headers {
-		tmpl, err := a.template.Parse(value)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
+		_ = value
+		if key == "Host" {
+			req.Header.Set(key, "test.test")
 		}
-
-		writer := &bytes.Buffer{}
-
-		err = tmpl.Execute(writer, req)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		req.Header.Set(key, writer.String())
 	}
-
 	a.next.ServeHTTP(rw, req)
 }
